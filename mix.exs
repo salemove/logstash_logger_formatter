7831dcd691aa3088b66f5b5fadf96ed1661964a5
Riivo Parts <riivo.parts@salemove.com>
2021-08-18 09:57:09 +0300

    Replace travis with GH Actions
    Travis is not working any more.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule LogstashLoggerFormatter.Mixfile do
     [
       app: :logstash_logger_formatter,
       version: "1.1.0",
-      elixir: "~> 1.7",
+      elixir: "~> 1.11",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
